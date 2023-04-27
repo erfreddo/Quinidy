449c78f580c3f06b5c5c329e732544f0d18ac471
erfreddo <federicolattanzio31@gmail.com>
2023-04-27 12:18:30 +0200

    piccole modifiche

--- pages/register.php
+++ pages/register.php
@@ -1,43 +1,53 @@
 <?php // Quinidy register page
-	include '../php/connection.php';
-	define("TITLE","Registrati a Quinidy");
-	session_start();
-	error_reporting(0);
+include '../php/connection.php';
+define("TITLE", "Registrati a Quinidy");
+session_start();
+error_reporting(0);
 
-	if (isset($_SESSION['nickname'])) { header("Location: mainpage.php"); }
+if (isset($_SESSION['nickname'])) {
+	header("Location: mainpage.php");
+}
 
-	if (isset($_POST['submit'])) {
-		$nickname = $_POST['nickname'];
-		$email = $_POST['email'];
-		$password = md5($_POST['password']);
-		$password2 = md5($_POST['password2']);
+if (isset($_POST['submit'])) {
+	$nickname = $_POST['nickname'];
+	$email = $_POST['email'];
+	$password = md5($_POST['password']);
+	$password2 = md5($_POST['password2']);
 
-		if ($password == $password2) {
-			$sql = "SELECT * FROM utenti WHERE email='$email'";
-			$result = mysqli_query($conn, $sql);
-			if (!$result->num_rows > 0) {
-				$sql = "INSERT INTO utenti (nickname, email, password)
+	if ($password == $password2) {
+		$sql = "SELECT * FROM utenti WHERE email='$email'";
+		$result = mysqli_query($conn, $sql);
+		if (!$result->num_rows > 0) {
+			$sql = "INSERT INTO utenti (nickname, email, password)
 						VALUES ('$nickname', '$email', '$password')";
-				$result = mysqli_query($conn, $sql);
-				if ($result) {
-					echo "<script>alert('Registrazione completata.')</script>";
-					$nickname = "";
-					$email = "";
-					$_POST['password'] = "";
-					$_POST['password2'] = "";
-					header("Location: login.php");
-				} else { echo "<script>alert('Errore, qualcosa non va.')</script>"; }
-			} else { echo "<script>alert('Email già registrata.')</script>"; }
-		} else { echo "<script>alert('La password non corrisponde.')</script>"; }
+			$result = mysqli_query($conn, $sql);
+			if ($result) {
+				echo "<script>alert('Registrazione completata.')</script>";
+				$nickname = "";
+				$email = "";
+				$_POST['password'] = "";
+				$_POST['password2'] = "";
+				header("Location: login.php");
+			} else {
+				echo "<script>alert('Errore, qualcosa non va.')</script>";
+			}
+		} else {
+			echo "<script>alert('Email già registrata.')</script>";
+		}
+	} else {
+		echo "<script>alert('La password non corrisponde.')</script>";
 	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="it">
-    <head><?php include 'layouts/headTags.php'; ?></head>
+
+	<head><?php include 'layouts/headTags.php'; ?></head>
+
 	<body class="all-bg purple-bg d-flex flex-column min-vh-100">
-	
+
 		<?php include("../pages/layouts/navbar.php") ?>
-		
+
 		<section class="margin-main text-center py-4">
 			<div class="container text-white">
 				<div class="row justify-content-center">
@@ -57,7 +67,9 @@
 								<div class="form-group d-flex mb-3">
 									<input type="password" class="form-control rounded-left" name="password2" placeholder="Conferma password" value="<?php echo $_POST['password2']; ?>" required>
 								</div>
-								<div class="text-md-right"> <p>Hai un account? <a href="login.php" id="button">Accedi</a>.</p> </div>
+								<div class="text-md-right">
+									<p>Hai un account? <a href="login.php" id="button">Accedi</a>.</p>
+								</div>
 								<div class="form-group d-flex justify-content-center">
 									<button type="submit" class="btn btn-hover color-3 rounded-pill submit" name="submit" id="button">Registrati</button>
 								</div>
@@ -66,9 +78,20 @@
 					</div>
 				</div>
 			</div>
-		</section>	
-		
+		</section>
+
 		<?php include("../pages/layouts/footer.php") ?>
+
+		<script>
+			$('input').on('keypress', function(event) {
+				var regex = new RegExp("^[a-zA-Z0-9]+$");
+				var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+				if (!regex.test(key)) {
+					event.preventDefault();
+					return false;
+				}
+			});
+		</script>
 		
-	</body>
+	</body> 
 </html>
